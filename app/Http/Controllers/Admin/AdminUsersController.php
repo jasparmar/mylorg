6df3df62e249550118ec0ac4d7197c14ddd8af90
webfocustech <webfocustech@gmail.com>
2024-04-11 19:08:43 +0530

    project added

---
+++ app/Http/Controllers/Admin/AdminUsersController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    // public $page_title;
+    protected $module_details;
+    protected $table_fields;
+    protected $page_limit;
+    protected $search_keyword;
+
+    public function __construct(Request $request)
+    {
+        $this->page_limit = 10; //Default Limit Set
+        $this->search_keyword = $request->keyword;
+        $this->module_details = array(
+            'page_title' => 'Admin Users',
+            'button_title' => 'Admin User',
+        );
+
+        $this->table_fields = array(
+            'id' => 'Id',
+            'name' => 'Name',
+            'guard_name' => 'Guard Nname',
+            'created_at' => 'Added Date',
+        );
+    }
+
+    public function index()
+    {
+        $data = [];
+        $data['page'] = 1;
+        $data['page_limit'] = $this->page_limit;
+        $data['fields'] = $this->table_fields;
+        $data['module_details'] = $this->module_details;
+        $data['keyword'] = $this->search_keyword;
+        return view('admin.adminuser.index',$data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
